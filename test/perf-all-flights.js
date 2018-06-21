6c26e2fd726a08c8c46eddbbbfe6c5813c3a220b
Robb Dempsey <robb.dempsey@gmail.com>
2018-06-21 17:12:27 -0500

    [WIP] old v new
    autocannon tests that will run against the flights routes of the old and new code

---
+++ test/perf-all-flights.js
@@ -0,0 +1,24 @@
+'use strict'
+
+const autocannon = require('autocannon')
+
+const url = 'http://localhost:9080/rest/api/flights'
+const postData = 'fromAirport=&toAirport=&fromDate=Thu%20Jun%2021%202018%2000%3A00%3A00%20GMT-0500%20(Central%20Daylight%20Time)&returnDate=Thu%20Jun%2021%202018%2000%3A00%3A00%20GMT-0500%20(Central%20Daylight%20Time)&oneWay=false'
+
+autocannon({
+  title: 'acmeair-all-flights',
+  url,
+  method: 'POST',
+  body: postData,
+  headers: {
+    'Content-Type': 'application/x-www-form-urlencoded'
+  },
+  connections: 10, //default
+  pipelining: 1, // default
+  duration: 10 // default
+}, (err, results) => {
+
+  console.log(results)
+
+})
+
---
+++ test/perf-flights-old-code.js
@@ -0,0 +1,25 @@
+'use strict'
+
+const autocannon = require('autocannon')
+
+const url = 'http://localhost:9080/rest/api/flights/queryflights'
+const postData = 'fromAirport=BOM&toAirport=AMS&fromDate=Thu%20Jun%2021%202018%2000%3A00%3A00%20GMT-0500%20(Central%20Daylight%20Time)&returnDate=Thu%20Jun%2021%202018%2000%3A00%3A00%20GMT-0500%20(Central%20Daylight%20Time)&oneWay=false'
+
+autocannon({
+  title: 'acmeair-flights-old-code',
+  url,
+  method: 'POST',
+  body: postData,
+  headers: {
+    'Content-Type': 'application/x-www-form-urlencoded',
+    'Cookie': 'sessionid=0bdaf762-2eed-4adc-ad75-674565f74bb5; loggedinuser=uid0%40email.com'
+  },
+  connections: 10, //default
+  pipelining: 1, // default
+  duration: 10 // default
+}, (err, results) => {
+
+  console.log(results)
+
+})
+
